b931fb5806bb87d24f8fd0ef3bd2c2cb25fccc85
author31 <wi072815.tw@gmail.com>
2026-03-16 17:08:38 +0800

    init

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# To have ros2.bridge extension worked with IsaacSim
-# Source the ROS environment setup file(s)
-# source /workspace/humble_ws/install/setup.bash
-# source /workspace/build_ws/install/local_setup.bash
-
-exec "$@"
